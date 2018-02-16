d4f8f057f1bb213aa06ee9c7241e9e86123ce811
endroid <info@endroid.nl>
2018-02-16 09:22:15 +0100

    Update tests

--- tests/FlusherTest.php
+++ tests/FlusherTest.php
@@ -9,12 +9,27 @@
 
 namespace Endroid\Flusher\Tests;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+use Endroid\Flusher\Flusher;
 use PHPUnit\Framework\TestCase;
 
 class FlusherTest extends TestCase
 {
-    public function testNoTestsYet()
+    public function testCreateFlusher()
     {
-        $this->assertTrue(true);
+        $config = Setup::createAnnotationMetadataConfiguration(['/'], true);
+        $entityManager = EntityManager::create([
+            'driver' => 'pdo_mysql',
+            'user' => 'root',
+            'password' => 'root',
+            'dbname' => 'flusher',
+        ], $config);
+
+        $flusher = new Flusher($entityManager);
+        $flusher->flush();
+        $flusher->finish();
+
+        $this->assertInstanceOf(Flusher::class, $flusher);
     }
 }
